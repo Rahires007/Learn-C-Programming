4932777650f145d402b4b1a7c9fe8c97a49e0c20
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 16:05:23 +0530

    Create String.c

---
+++ String.c
@@ -0,0 +1,11 @@
+//String program
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char Name[20];
+printf("\nEnter your name.....");
+scanf("\n%s",&Name);
+printf("\nHello ....%s",Name);
+return 0;
+}
