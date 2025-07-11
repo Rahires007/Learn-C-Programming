f8ec38bd1824e678ad07977088e75bd8fbf44ea7
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 16:04:12 +0530

    Create Operation.c

---
+++ Operation.c
@@ -0,0 +1,14 @@
+
+//All arithmetic operations 
+#include<stdio.h>
+int main()
+{
+  float Num1,Num2;
+printf("\nEnter 2 no....");
+scanf("\n%f%f",&Num1,&Num2);
+printf("\nAdd =%f",Num1+Num2);
+printf("\nSub =%f",Num1-Num2);
+printf("\nMulti =%f",Num1*Num2);
+printf("\nDiv =%f",Num1/Num2);
+return 0;
+}
